eadaf4f9ca98bff04d6f923a438fcdca3199911c
surajbhatt27 <bhattsuraj0066@gmail.com>
2026-04-15 20:28:17 +0530

    style: improve layout and styling for Molar Mass components and ElementRow

--- src/components/ElementRow.tsx
+++ src/components/ElementRow.tsx
@@ -13,12 +13,12 @@ type Props = {
 
 export default function ElementRow({index, row, onRemove, onChange}: Props) {
     return (
-        <div className=" mt-2 flex flex-col sm:flex-row gap-2">
+        <div className="flex flex-col gap-2 p-2 bg-gray-50 rounded-xl">
             <select name="elements"
             value={row.element}
             onChange={(e)=> onChange(index, "element", e.target.value)}
-            className="bg-blue-200 px-4 py-2 rounded-lg border border-gray-500 w-fit sm:w-full">
-                <option value="H" className="w">H</option>
+            className="bg-blue-50 px-3 py-2 rounded-lg border border-gray-300 w-full text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <option value="H">H</option>
                 <option value="O">O</option>
                 <option value="Na">Na</option>
                 <option value="Cl">Cl</option>
@@ -28,9 +28,10 @@ export default function ElementRow({index, row, onRemove, onChange}: Props) {
             value={row.quantity}
             onChange={(e)=> onChange(index, "quantity", Number(e.target.value))}
             placeholder="Enter quantity" 
-            className="px-4 py-2 border rounded-2xl w-full" 
+            className="px-3 py-2 border border-gray-300 rounded-lg w-full text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-blue-500"
+                min="1" 
             />
-            <button onClick={()=>onRemove(index)} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-xl transition ml-2 w-full sm:w-auto cursor-pointer">Remove</button>
+            <button onClick={()=>onRemove(index)} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition w-full cursor-pointer text-sm sm:text-base">Remove</button>
         </div>
     )
 }
